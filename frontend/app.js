822526a86f6b5920a29ed59be748ca764d668929
shadowsports <startearly27@gmail.com>
2026-07-10 18:31:17 -0700

    Display shadow score in HTML element

--- frontend/app.js
+++ frontend/app.js
@@ -2,4 +2,8 @@ console.log("Shadow Sports Intelligence is running.");
 
 const shadowScore = 94;
 
+const scoreDisplay = document.getElementById("score");
+
+scoreDisplay.innerHTML = shadowScore + "/100";
+
 console.log("Shadow Intelligence Score:", shadowScore);
